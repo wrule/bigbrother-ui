a3dc63878924913d7ebf952a7495836ad9fb119a
jimao <jimao@jimaodeMacBook-Pro.local>
2021-07-26 10:57:02 +0800

    开始修改XCodeEditor

--- src/components/codeEditor/index.tsx
+++ src/components/codeEditor/index.tsx
@@ -5,8 +5,6 @@ import style from './index.module.scss';
 import * as Monaco from 'monaco-editor';
 const monaco = require('monaco-editor/esm/vs/editor/editor.api');
 
-Monaco
-
 @Component
 export default class XCodeEditor extends Vue {
   /**
@@ -32,21 +30,6 @@ export default class XCodeEditor extends Vue {
       this.code = nv;
       this.editor?.getModel()?.setValue(this.code);
     }
-  }
-
-  @Watch('lang')
-  private handleLangChange(nv: string) {
-    const model = this.editor.getModel();
-    if (model) {
-      monaco.editor.setModelLanguage(model, this.lang);
-    }
-  }
-
-  @Watch('readonly')
-  private handleReadOnlyChange(nv: boolean) {
-    this.editor.updateOptions({
-      readOnly: nv,
-    });
   }
 
   @Emit('input')
@@ -76,26 +59,6 @@ export default class XCodeEditor extends Vue {
       this.emitInput(this.code);
       this.emitChange(this.code);
     });
-  }
-
-  public FindText(text: string) {
-    const matches: any[] = (this.editor.getModel() as any).findMatches(text);
-    if (matches.length > 0) {
-      const first = matches[0];
-      const range = first.range;
-      this.editor.setSelection(range);
-      this.editor.getAction('actions.find').run();
-      const lineNumber = range.startLineNumber;
-      this.editor.revealLine(lineNumber);
-    }
-  }
-
-  public Reset() {
-
-  }
-
-  public SetScrollTop(top: number) {
-    this.editor.setScrollPosition({ scrollTop: top });
   }
 
   private mounted() {
